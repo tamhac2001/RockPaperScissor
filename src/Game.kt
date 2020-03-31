18cabfcad77577478c0bbab76106aaeab8d9322e
Tam Hắc <tamhac2001@gmail.com>
2020-03-31 17:43:13 +0700

    FINISH

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,4 @@
+import java.awt.SystemTray
 import java.util.*
 
 fun getGameChoice(optionParam: Array<String>) =
@@ -11,19 +12,44 @@ fun getPlayerChoice(optionParam: Array<String>) :String{
         for(i in 1..3){
             println("$i. ${optionParam.get(i-1)}")
         }
-        println("Please chose one:")
+        print("Please chose one:")
         playerInput= sc.nextInt()
         if (playerInput!=null && playerInput <= optionParam.size){
             isvalidChoice = true
             playerChoice = optionParam.get(playerInput-1)
         }
+        else println("Please enter valid choice")
     }
     return playerChoice
 }
+fun printResult(gameChoice:String , playerChoice:String){
+    if(gameChoice==playerChoice)
+        println("Tie")
+    else{
+        if ((gameChoice=="Rock" && playerChoice=="Scissor") ||
+            (gameChoice=="Scissor" && playerChoice=="Paper")||
+            (gameChoice=="Paper" && playerChoice=="Rock"))
+            println("You lose")
+        else
+            println("You win")
+    }
+}
+fun playAgain() : Boolean{
+    println("Play again?(Y/N)")
+    val sc = Scanner(System.`in`)
+    var playerAnswer:Char = sc.next().single()
+    if(playerAnswer.toLowerCase()=='y')
+        return true
+    else return false
+}
 fun main(args: Array<String>){
-    val option = arrayOf("Rock","Paper","Scissor")
-    val gameChoice = getGameChoice(option)
-    val playerChoice = getPlayerChoice(option)
-    println("$gameChoice")
-    println("$playerChoice")
+    do{
+        val option = arrayOf("Rock","Paper","Scissor")
+        val gameChoice = getGameChoice(option)
+        val playerChoice = getPlayerChoice(option)
+        println("Game choose $gameChoice")
+        println("You choose $playerChoice")
+        printResult(gameChoice,playerChoice)
+    }while (playAgain())
+
 }
